1f72e2515ab6816c53857d642478f996ae03450f
king <>
2025-04-28 16:08:00 +0800

    feat: 增加用时

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@
 import { cutFile } from "./cutFile";
 
 document.getElementById("fileInput").onchange = async (e) => {
+  const startTime = performance.now();
   console.log("🚀 ~ document.getElementById ~ 分片开始:");
   console.log("分片中...");
   const file = e.target.files[0];
   const chunks = await cutFile(file);
   console.log("🚀 ~ document.getElementById ~ chunks:", chunks);
   console.log("🚀 ~ document.getElementById ~ 分片结束:");
+  const endTime = performance.now();
+  const duration = ((endTime - startTime) / 1000).toFixed(2);
+  console.log(
+    "🚀 ~ document.getElementById ~ 分片结束, 总用时:",
+    duration,
+    "秒"
+  );
 };
